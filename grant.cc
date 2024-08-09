1d1cc3708cdd61c36fb1b2cd7d43f9f524e6cc33
wesleywang <wesleywang@insta360.com>
2024-08-09 10:24:51 +0800

    abc

--- grant.cc
+++ grant.cc
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"grant"<<endl;
-}
+ 
+ int main()
+ {
+    
+ }
--- main.cc
+++ main.cc
@@ -3,5 +3,9 @@ using namespace std;
 int main()
 {
     cout<<"hello world"<<endl;
+<<<<<<< HEAD
     cout<<"grant"<<endl;
+=======
+    
+>>>>>>> abc
 }
